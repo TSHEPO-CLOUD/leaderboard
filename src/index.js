3df6197b1a8674ec845de9d3b5628a769a12928b
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-07-29 17:44:46 +0200

    Add html tags in js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+
+const addForm = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const scoreInput = document.querySelector('#score');
+const refresh = document.querySelector('button');
+const list = document.querySelector('.score-list');
+
